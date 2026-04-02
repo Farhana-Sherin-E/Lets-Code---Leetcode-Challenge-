421f5bafd0893b684c6fbddad589be12babd9b6f
Farhana Sherin E <farhanaedavath@gmail.com>
2026-04-02 11:06:47 +0530

    Implement buyChoco method to calculate remaining money

---
+++ Day19/chocolate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        sum = prices[0] + prices[1]
+        if sum <= money:
+            return money - sum
+        else:
+            return money 
